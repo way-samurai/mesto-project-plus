64f6bc99018429bfcd7b949bc8535fd0d61e23bd
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-05-03 01:40:19 +0300

    Добавил описание запросов и передал им контролееры в usersRouter

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,5 +1,10 @@
+import { createUsers, getUserById, getUsers } from 'controllers/users';
 import { Router } from 'express';
 
 const usersRouter = Router();
+
+usersRouter.get('/', getUsers);
+usersRouter.get('userId', getUserById);
+usersRouter.post('/', createUsers);
 
 export default usersRouter;
