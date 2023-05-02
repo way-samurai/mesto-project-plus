fff0588ea27b383d5606936943d37a5a8e35dc56
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-05-02 23:38:36 +0300

    Добавил библиотеку mangoose и возможность подключения к MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-import express from 'express';
+import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = "mongodb://localhost:27017/mestodb" } =
+  process.env;
 
 const app = express();
 
-const start = () => {
+const start = async () => {
   try {
+    // подключаемся к серверу MongoiDB
+    await mongoose.connect(DB_URL);
+    
     app.listen(PORT);
     console.log("Сервер запущен", PORT);
   } catch (err) {
