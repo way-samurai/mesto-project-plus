f2208f06353ad110beb0932cb404e91029fc5a7f
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-05-06 02:20:31 +0300

    Создал роут для логина и регистрации, а также удалил старый обработчик создания пользователя

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import {
-  createUsers, getUserById, getUsers, patchUserAvatar, patchUserData,
+  createUsers, getUserById, getUsers, login, patchUserAvatar, patchUserData,
 } from '../controllers/users';
 
 const usersRouter = Router();
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/:userId', getUserById);
-usersRouter.post('/', createUsers);
+usersRouter.post('/sigin', login);
+usersRouter.post('/signup', createUsers);
 usersRouter.patch('/me', patchUserData);
 usersRouter.patch('/me/avatar', patchUserAvatar);
 
