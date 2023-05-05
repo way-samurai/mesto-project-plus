bc280777c2c42a04630f272a1beee913a11f5961
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-05-06 02:52:35 +0300

    Удалил хардкод фейковой авторизации

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes';
-import { testUserId } from './utils/testUser';
 import errorHandler from './middleware/ErrorHandingMiddleware';
 import { createUsers, login } from './controllers/users';
 
@@ -9,7 +8,6 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 
 const app = express();
 app.use(express.json());
-app.use(testUserId);
 app.post('/sigin', login);
 app.post('/signup', createUsers);
 app.use(router);
--- src/utils/testUser.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-import { IAppRequest } from './types';
-
-export const testUserId = (req: Request, res: Response, next: NextFunction) => {
-  (req as IAppRequest).user = {
-    _id: '64552e27dc177748e26dc244',
-  };
-  next();
-};
